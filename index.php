2e35be7224b3a19c15721b2e1ce7032fe3f8dba0
yamasan115 <115602782+yamasan115@users.noreply.github.com>
2022-11-14 11:41:18 +0900

    追加：商品画像

--- index.php
+++ index.php
@@ -32,51 +32,51 @@
     <div class="container-fluid">
         <!-- 見出し：おすすめ -->
         <div class="row my-5">
-            <div class="col-6 offset-3">
-                <h1 class="text-center h1">TOPICS</h1>
-                <p class="text-center">＼＼スタッフのおすすめ／／</p>
+            <div class="col-9 offset-0">
+                <h1 class="text-left text-weight-bold">TOPICS</h1>
+                <p class="text-left h5">スタッフのおすすめ</p>
             </div>
         </div>
 
         <!-- 商品一覧 -->
-        <div class="row row-cols-2 row-cols-sm-3 row-cols-lg-4 g-4 mx-auto">
+        <div class="row row-cols-2 row-cols-sm-2 row-cols-lg-4 g-3 mx-auto">
 
-            <div class="col" style="max-width: 300px;">
-                <div class="card h-100">
-                    <img src="img/janbotoron.jpg" class="card-img-top" alt="">
+            <div class="col" style="">
+                <div class="card">
+                    <img src="img/janbotoron.jpg" class="card-img-top img-fluid" alt="">
                     <div class="card-body" >
                         <h3 class="card-title text-center">商品タイトル</h3>
-                        <p class="card-text text-center">1,000円</p>
+                        <p class="card-text text-center h6">1,000円</p>
                     </div>
                 </div>
             </div>
 
-            <div class="col" style="max-width: 300px;">
-                <div class="card h-100">
-                    <img src="img/janbotoron.jpg" class="card-img-top " alt="">
+            <div class="col" style="">
+                <div class="card ">
+                    <img src="img/セット3.png" class="card-img-top img-fluid " alt="">
                     <div class="card-body">
                         <h3 class="card-title text-center">商品タイトル</h3>
-                        <p class="card-text text-center">1,000円</p>
+                        <p class="card-text text-center h6">1,000円</p>
                     </div>
                 </div>
             </div>
 
-            <div class="col" style="max-width: 300px;">
-                <div class="card h-100">
-                    <img src="img/janbotoron.jpg" class="card-img-top " alt="">
+            <div class="col" style="">
+                <div class="card ">
+                    <img src="img/セット1.jpg" class="card-img-top img-fluid" alt="">
                     <div class="card-body">
                         <h3 class="card-title text-center">商品タイトル</h3>
-                        <p class="card-text text-center">1,000円</p>
+                        <p class="card-text text-center h6">1,000円</p>
                     </div>
                 </div>
             </div>
 
-            <div class="col" style="max-width: 300px;">
-                <div class="card h-100">
-                    <img src="img/janbotoron.jpg" class="card-img-top" alt="">
+            <div class="col" style="">
+                <div class="card ">
+                    <img src="img/セット1.png" class="card-img-top img-fluid  " alt="">
                     <div class="card-body">
                         <h3 class="card-title text-center">商品タイトル</h3>
-                        <p class="card-text text-center">1,000円</p>
+                        <p class="card-text text-center h6">1,000円</p>
                     </div>
                 </div>
             </div>
