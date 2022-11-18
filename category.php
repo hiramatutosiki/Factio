1ee93393256c7ea58611a5e44ce96024fbdb9990
yamasan115 <115602782+yamasan115@users.noreply.github.com>
2022-11-18 11:07:24 +0900

    追加：カテゴリページの編集

--- category.php
+++ category.php
@@ -16,7 +16,7 @@
     require_once(__DIR__ . '/header.php');
     ?>
 
-    <div class="container-fluid">
+    <div class="container-fluid w-75">
         <!-- 見出し -->
         <div class="row my-5">
             <div class="col-6 offset-3">
@@ -25,50 +25,60 @@
         </div>
 
         <!-- 商品一覧 -->
-        <div class="row row-cols-2 row-cols-sm-3 row-cols-lg-4 g-4 mx-auto">
+        <div class="row row-cols-2 row-cols-sm-2 row-cols-lg-4 g-3 mx-auto">
 
-            <div class="col" style="max-width: 300px;">
-                <div class="card h-100">
-                    <img src="img/セット名_detail2" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">title</h3>
-                        <p class="card-text">1000円</p>
-                    </div>
-                </div>
+    <div class="col" style="">
+        <div class="card ">
+            <img src="img/サンプル.jpg" class="card-img-top img-fluid" alt="">
+            <div class="card-body" >
+                <h3 class="card-title text-center">商品タイトル</h3>
+                <p class="card-text text-center h6">1,000円</p>
             </div>
+         </div>
+    </div>
 
-            <div class="col" style="max-width: 300px;">
-                <div class="card h-100">
-                    <img src="image/main.jpg" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">title</h3>
-                        <p class="card-text">1000円</p>
-                    </div>
-                </div>
+    <div class="col" style="">
+        <div class="card ">
+            <img src="img/セット1サンプル.png" class="card-img-top img-fluid " alt="">
+            <div class="card-body">
+                <h3 class="card-title text-center">商品タイトル</h3>
+                <p class="card-text text-center h6">1,000円</p>
             </div>
-
-            <div class="col" style="max-width: 300px;">
-                <div class="card h-100">
-                    <img src="image/main.jpg" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">title</h3>
-                        <p class="card-text">1000円</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col" style="max-width: 300px;">
-                <div class="card h-100">
-                    <img src="image/main.jpg" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">title</h3>
-                        <p class="card-text">1000円</p>
-                    </div>
-                </div>
-            </div>
-
         </div>
     </div>
+
+    <div class="col" style="">
+        <div class="card ">
+        <img src="img/セット1サンプル.jpg" class="card-img-top img-fluid" alt="">
+            <div class="card-body">
+             <h3 class="card-title text-center">商品タイトル</h3>
+             <p class="card-text text-center h6">1,000円</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col" style="">
+        <div class="card ">
+        <img src="img/セット1サンプル.png" class="card-img-top img-fluid  " alt="">
+            <div class="card-body">
+                <h3 class="card-title text-center">商品タイトル</h3>
+                <p class="card-text text-center h6">1,000円</p>
+         </div>
+        </div>
+    </div>
+
+    <div class="col" style="">
+        <div class="card ">
+        <img src="img/セット1サンプル.png" class="card-img-top img-fluid  " alt="">
+            <div class="card-body">
+                <h3 class="card-title text-center">商品タイトル</h3>
+                <p class="card-text text-center h6">1,000円</p>
+            </div>
+        </div>
+    </div>
+
+     </div>
+ </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
