c17411202839e2ba9b76411945bdb2a2af9459cb
yamasan115 <115602782+yamasan115@users.noreply.github.com>
2022-11-29 15:03:19 +0900

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
 
     <title>Factio</title>
     <style>
-        .bg-img{
-            background-image:url(img/rei.jpg);
-            background-size:cover;
-            background-position:center 60%;
+        .bg-img {
+            background-image: url(img/rei.jpg);
+            background-size: cover;
+            background-position: center 60%;
         }
     </style>
 </head>
@@ -23,10 +23,10 @@
     require_once(__DIR__ . '/header.php');
     ?>
     <!-- ジャンボトロン -->
-    <div class ="bg-img p-5">
+    <div class="bg-img p-5">
         <div class="container">
-            <img src="img/rei.jpg"class="img-fluid">
-        </div>      
+            <img src="img/rei.jpg" class="img-fluid">
+        </div>
     </div>
 
     <div class="container-fluid w-75">
@@ -40,6 +40,7 @@
 
         <!-- 商品一覧 -->
         <div class="row row-cols-2 row-cols-sm-2 row-cols-lg-4 g-3 mx-auto">
+<<<<<<< HEAD
 
             <div class="col" style="">
                 <div class="card ">
@@ -101,6 +102,14 @@
                 </div>
             </div>
             
+=======
+            <?php
+            require_once 'DBManager.php';
+            $dbmng = new DBManager();
+            $dbmng->getItemByCategory('クリスマス');
+            $dbmng->getItemByCategory('年末年始');
+            ?>
+>>>>>>> main
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
